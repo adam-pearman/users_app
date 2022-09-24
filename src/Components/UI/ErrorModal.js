6867f856a019b53a2f4cff99ee60351b8e5db51a
Adam <104145646+adam-pearman@users.noreply.github.com>
2022-09-24 18:19:01 +0100

    use portals

--- src/Components/UI/ErrorModal.js
+++ src/Components/UI/ErrorModal.js
@@ -1,24 +1,36 @@
 import styles from './ErrorModal.module.css'
 import BaseCard from "./BaseCard";
 import BaseButton from "./BaseButton";
+import {createPortal} from "react-dom";
+
+const ModalBackdrop = (props) => {
+    return (
+        <div className={styles.backdrop} onClick={props.onDismissModal}/>
+    )
+}
+
+const ModalOverlay = (props) => {
+    return (
+        <BaseCard className={styles.modal}>
+            <header className={styles.header}>
+                <h2>{props.title}</h2>
+            </header>
+            <div className={styles.content}>
+                {props.message}
+            </div>
+            <footer className={styles.actions}>
+                <BaseButton type="button" onClick={props.onDismissModal}>Okay</BaseButton>
+            </footer>
+        </BaseCard>
+    )
+}
 
 const ErrorModal = (props) => {
     return (
         <>
-            <div className={styles.backdrop} onClick={props.onDismissModal}/>
-            <BaseCard className={styles.modal}>
-                <header className={styles.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={styles.content}>
-                    {props.message}
-                </div>
-                <footer className={styles.actions}>
-                    <BaseButton type="button" onClick={props.onDismissModal}>Okay</BaseButton>
-                </footer>
-            </BaseCard>
+            {createPortal(<ModalBackdrop onDismissModal={props.onDismissModal}/>, document.getElementById('backdrop-root'))}
+            {createPortal(<ModalOverlay title={props.title} message={props.message} onDismissModal={props.onDismissModal}/>, document.getElementById('overlay-root'))}
         </>
-
     )
 }
 
